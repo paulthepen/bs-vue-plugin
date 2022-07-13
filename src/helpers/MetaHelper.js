867fcefe131ed551ee0120e372036f018c208dfa
Paul Charpie <paul@hawkinscomputerservices.com>
2022-07-13 16:29:34 -0400

    removed unused helper

--- src/helpers/MetaHelper.js
+++
@@ -1,7 +0,0 @@
-export const setMetaData(data) => {
-    return data;
-}
-
-export const setTitle(title) => {
-    
-}
--- src/router.js
+++ src/router.js
@@ -6,17 +6,18 @@ const pages = [
   {
     name: "unit-search",
     component: unitSearch,
-    meta: { 
-        title: "Search for a Cabin!",
-        metaTags: [{
-            name: 'description',
-            content: 'Search for a cabin'
+    meta: {
+      title: "Search for a Cabin!",
+      metaTags: [
+        {
+          name: "description",
+          content: "Search for a cabin",
         },
         {
-            property: 'og:description',
-            content: 'Search for a cabin'
+          property: "og:description",
+          content: "Search for a cabin",
         },
-    ]
+      ],
     },
   },
   { name: "unit-details/:unitName", component: unitDetails },
@@ -43,45 +44,45 @@ export const initializeRouter = (pathRoot) => {
 };
 
 export const setMetaData = (to, from, next) => {
-    const nearestWithTitle = to.matched
-      .slice()
-      .reverse()
-      .find((r) => r.meta && r.meta.title);
-    const nearestWithMeta = to.matched
-      .slice()
-      .reverse()
-      .find((r) => r.meta && r.meta.metaTags);
-    const previousNearestWithMeta = from.matched
-      .slice()
-      .reverse()
-      .find((r) => r.meta && r.meta.metaTags);
+  const nearestWithTitle = to.matched
+    .slice()
+    .reverse()
+    .find((r) => r.meta && r.meta.title);
+  const nearestWithMeta = to.matched
+    .slice()
+    .reverse()
+    .find((r) => r.meta && r.meta.metaTags);
+  const previousNearestWithMeta = from.matched
+    .slice()
+    .reverse()
+    .find((r) => r.meta && r.meta.metaTags);
 
-    if (nearestWithTitle) {
-      document.title = nearestWithTitle.meta.title;
-    } else if (previousNearestWithMeta) {
-      document.title = previousNearestWithMeta.meta.title;
-    }
+  if (nearestWithTitle) {
+    document.title = nearestWithTitle.meta.title;
+  } else if (previousNearestWithMeta) {
+    document.title = previousNearestWithMeta.meta.title;
+  }
 
-    Array.from(document.querySelectorAll("[data-vue-router-controlled]")).map(
-      (el) => el.parentNode.removeChild(el)
-    );
-    if (!nearestWithMeta) {
-      return next();
-    }
+  Array.from(document.querySelectorAll("[data-vue-router-controlled]")).map(
+    (el) => el.parentNode.removeChild(el)
+  );
+  if (!nearestWithMeta) {
+    return next();
+  }
 
-    nearestWithMeta.meta.metaTags
-      .map((tagDef) => {
-        const tag = document.createElement("meta");
+  nearestWithMeta.meta.metaTags
+    .map((tagDef) => {
+      const tag = document.createElement("meta");
 
-        Object.keys(tagDef).forEach((key) => {
-          tag.setAttribute(key, tagDef[key]);
-        });
+      Object.keys(tagDef).forEach((key) => {
+        tag.setAttribute(key, tagDef[key]);
+      });
 
-        tag.setAttribute("data-vue-router-controlled", "");
+      tag.setAttribute("data-vue-router-controlled", "");
 
-        return tag;
-      })
+      return tag;
+    })
 
-      .forEach((tag) => document.head.appendChild(tag));
-    next();
-}
+    .forEach((tag) => document.head.appendChild(tag));
+  next();
+};
