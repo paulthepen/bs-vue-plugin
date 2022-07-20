8dea700291a8813a35de49a755ea331abcb7e0d6
Paul Charpie <paul@hawkinscomputerservices.com>
2022-07-20 11:21:12 -0400

    added custom styling to header

--- src/components/layout/ListHeader.vue
+++ src/components/layout/ListHeader.vue
@@ -1,24 +1,32 @@
 <template>
-    <div class="flex-fluid">
+    <div class="flex-fluid" :style="customStyles">
         Header
         <slot></slot>
     </div>
 </template>
 
 <script>
-    export default {
-        props:['options'],
-        components: {},
-        data() {
-            return {
-                
-            }
-        },
-        computed: {
-            //custom height option
-        }, 
-        methods: {
-            
+export default {
+    props: ['options'],
+    components: {},
+    data() {
+        return {
+
         }
+    },
+    computed: {
+        customStyles() {
+            const styles = this.options?.styles;
+
+            if (!styles) {
+                return "";
+            } else {
+                return styles;
+            }
+        }
+    },
+    methods: {
+
     }
+}
 </script>
